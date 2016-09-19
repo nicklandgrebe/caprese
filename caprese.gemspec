a6812e2d94bb3ecbfec8249f68863f8a99032223
Nick Landgrebe <nick@landgre.be>
2016-09-19 11:27:55 -0500

    Add database cleaning to specs

--- caprese.gemspec
+++ caprese.gemspec
@@ -27,6 +27,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency 'bundler', '~> 1.12'
   spec.add_development_dependency 'factory_girl'
+  spec.add_development_dependency 'database_cleaner'
   spec.add_development_dependency 'pry-byebug'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'responders', '~> 2.0'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ ENV['RAILS_ENV'] ||= 'test'
 ENV['RAILS_ROOT'] = File.expand_path('../dummy',  __FILE__)
 require File.expand_path('../dummy/config/environment', __FILE__)
 
+require 'database_cleaner'
 require 'factory_girl'
 require 'pry'
 require 'rspec/rails'
@@ -15,4 +16,24 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Responses
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
