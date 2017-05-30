6e2ad8e0b3b39b5aa7f7a9527625a3ab0cac9ef5
Nicholas Landgrebe <nick@landgre.be>
2017-05-30 12:24:12 -0400

    Add namespaced module helper

--- lib/caprese/concerns/versioning.rb
+++ lib/caprese/concerns/versioning.rb
@@ -4,7 +4,23 @@ module Caprese
   module Versioning
     extend ActiveSupport::Concern
 
+    # Get full namespaced module name of object (class)
+    #
+    # @example
+    #   namespaced_module('OrdersController') => 'API::V1::OrdersController'
+    #
+    # @param [String] suffix the string to append, hypothetically a module in
+    #  this version's module space
+    # @return [String] the namespaced modulized name of the suffix passed in
+    def namespaced_module(suffix = nil)
+      name = (self.is_a?(Class) ? self : self.class).name.deconstantize
+
+      name + (suffix ? "::#{suffix}" : '')
+    end
+
     # Get versioned module name of object (class)
+    # @note The difference between namespaced and versioned module names is that if an isolated_namespace
+    #   is present, version_module will return a module name without the isolated namespace
     #
     # @example
     #   version_module('OrdersController') => 'API::V1::OrdersController'
@@ -13,11 +29,11 @@ module Caprese
     #  this version's module space
     # @return [String] the versioned modulized name of the suffix passed in
     def version_module(suffix = nil)
-      name = (self.is_a?(Class) ? self : self.class).name.deconstantize
+      name = namespaced_module(suffix)
 
       name = name.gsub("#{Caprese.config.isolated_namespace}::", '') if Caprese.config.isolated_namespace
 
-      name + (suffix ? "::#{suffix}" : '')
+      name
     end
 
     # Get versioned path (url)
--- spec/versioning_spec.rb
+++ spec/versioning_spec.rb
@@ -1,6 +1,27 @@
 require 'spec_helper'
 
 describe 'Versioning helpers', type: :helper do
+  describe '#namespaced_module' do
+    let!(:versioned_class) do
+      API::V1::PostsController
+    end
+
+    let(:output) { versioned_class.namespaced_module }
+
+    it 'returns the versioning modules of the class' do
+      expect(output).to eq('API::V1')
+    end
+
+    context 'config.isolate_namespace' do
+      before { Caprese.config.isolated_namespace = API }
+      after { Caprese.config.isolated_namespace = nil }
+
+      it 'returns the versioning modules of the class without the isolated namespace' do
+        expect(output).to eq('API::V1')
+      end
+    end
+  end
+
   describe '#version_module' do
     let!(:versioned_class) do
       API::V1::PostsController
